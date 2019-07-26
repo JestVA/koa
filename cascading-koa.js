cc9329ad69cc268cd449f4cc8b1aaec40e73bb2d
Dorin <d.dumitrascuta@gmail.com>
2019-07-26 01:07:18 +0100

    play with prettierand code formating

--- cascading-koa.js
+++ cascading-koa.js
@@ -4,24 +4,24 @@ const app = new Koa()
 // loger 
 
 app.use(async (ctx, next) => {
-	await next()
-	const rt = ctx.response.get('X-Response-Time') // getter
-	console.log(`${ctx.method} ${ctx.url} - ${rt}`)
+    await next()
+    const rt = ctx.response.get('X-Response-Time') // getter
+    console.log(`${ctx.method} ${ctx.url} - ${rt}`)
 })
 
 // x-response-time
 
 app.use(async (ctx, next) => {
-	const start = Date.now()
-	await next()
-	const ms = Date.now() - start
-	ctx.set('X-Response-Time', `${ms}ms`) // setter
+    const start = Date.now()
+    await next()
+    const ms = Date.now() - start
+    ctx.set('X-Response-Time', `${ms}ms`) // setter
 })
 
 // response 
 
 app.use(async ctx => {
-	ctx.body = "Hello nerd"
+    ctx.body = "Hello nerd"
 })
 
 app.listen(3001)
--- hello-koa.js
+++ hello-koa.js
@@ -2,7 +2,7 @@ const Koa = require('koa');
 const app = new Koa();
 
 app.use(async ctx => {
-	ctx.body = 'Hello Sweet World';
+    ctx.body = 'Hello Sweet World';
 });
 
 app.listen(3000);
