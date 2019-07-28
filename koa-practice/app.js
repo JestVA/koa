ab05942b76d23c1c6e136ff7f9ba68b94a8228d7
Dorin <d.dumitrascuta@gmail.com>
2019-07-28 18:06:11 +0100

    add koa-router, koa-json, and refactor code

--- koa-practice/app.js
+++ koa-practice/app.js
@@ -1,5 +1,22 @@
 const Koa = require('koa')
+const KoaRouter = require('koa-router')
+const json = require('koa-json') 
+
 const app = new Koa()
+const router = new KoaRouter()
+
+// Json prettier Middleware
+app.use(json()) // using the package
 
 app.use(async ctx => ctx.body = {msg: 'Hello Dori'}) // like you would do for an API 
+
+
+
+
+
+
+
+
+// Router Middleware
+
 app.listen(3000, () => console.log('Server started...'))
