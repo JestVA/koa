372d11e390fbc25f17eb3c7b38985c88297554fc
Dorin <d.dumitrascuta@gmail.com>
2019-07-28 18:16:23 +0100

    Add a route, and view with index extending template with koa-ejs

--- koa-practice/app.js
+++ koa-practice/app.js
@@ -1,6 +1,8 @@
 const Koa = require('koa')
 const KoaRouter = require('koa-router')
 const json = require('koa-json') 
+const path = require('path')
+const render = require('koa-ejs')
 
 const app = new Koa()
 const router = new KoaRouter()
@@ -10,6 +12,20 @@ app.use(json()) // using the package
 
 // Simple middleware example
 // app.use(async ctx => ctx.body = {msg: 'Hello Dori'}) // like you would do for an API 
+
+render(app, {
+    root: path.join(__dirname, 'views'),
+    layout: 'layout',
+    viewExtension: 'html',
+    cache: false,
+    debug: false
+})
+
+// Create route for index page
+
+router.get('/', async ctx => {
+    await ctx.render('index')
+})
 
 router.get('/test', ctx => ctx.body = 'Hello Test')
 
